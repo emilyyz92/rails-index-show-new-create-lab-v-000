00c13d5122123d5829222662c1386d76483d4cec
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-22 16:57:55 +0000

    automatically backed up by learn

--- db/migrate/20180622165651_create_coupon_table.rb
+++ db/migrate/20180622165651_create_coupon_table.rb
@@ -1,6 +1,8 @@
 class CreateCouponTable < ActiveRecord::Migration
   def change
-    create_table :coupon_tables do |t|
+    create_table :coupons do |t|
+      t.string :coupon_code
+      t.string :store
     end
   end
 end
